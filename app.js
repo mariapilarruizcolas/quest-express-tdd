a5c6316714f539e544d14823b462afaf75943aad
Pili <mariapilar.ruizcolas@neuf.fr>
2023-01-07 10:54:57 +0100

    First express test ok

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+require("dotenv").config();
+
+
+const message = (req, res) => {
+    res.status(200).json({ message: "Hello World!" });
+};
+
+app.get("/", message);
 
 module.exports = app;
